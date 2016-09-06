d16767c31e1c906913793e9ba2b868d4faa4b10f
James Reidy <jreidy@cornell.edu>
2016-09-06 16:16:09 -0400

    check and reject malformed directory structure on s3; put filenames in quotes

--- seapapers_s3_csv.sh
+++ seapapers_s3_csv.sh
@@ -6,24 +6,29 @@ echo "publication,year,volis,part,extension,title,url"
 
 aws s3 ls seapapers.library.cornell.edu/issues/ --recursive | while read line;
   do
-    sizecheck="[0-9]+[0-9] issues\/"
-    if [[ $line =~ $sizecheck ]]; then
-      # eliminate directories and empty files - size 0
-      path="[0-9]+ (issues\/.*)$"
-      if [[ $line =~ $path ]]; then
-        pathname="${BASH_REMATCH[1]}"
-        parts="(issues)\/([^\/]*)\/([0-9]+)\/([^\/]*)\/([^\/]*)\/(.*)\.(.*)$"
-        if [[ $pathname =~ $parts ]]; then
-          publication="${BASH_REMATCH[2]}"
-          year="${BASH_REMATCH[3]}"
-          volis="${BASH_REMATCH[4]}"
-          part="${BASH_REMATCH[5]}"
-          title="${BASH_REMATCH[6]}"
-          extension="${BASH_REMATCH[7]}"
-          url="https://s3.amazonaws.com/seapapers.library.cornell.edu/$pathname"
-          echo "$publication,$year,$volis,$part,$extension,$title,$url"
+    formcheck="([0-9\-]+) ([0-9\:]+)[ ]+([0-9]+) issues\/([a-z\-]+)\/([0-9\:]+)\/([^\/]+)\/([^\/]+)\/(.+)\.([a-zA-Z]+)$"
+    if [[ $line =~ $formcheck ]]; then
+      sizecheck="[0-9]+[0-9] issues\/"
+      if [[ $line =~ $sizecheck ]]; then
+        # eliminate directories and empty files - size 0
+        path="[0-9]+ (issues\/.*)$"
+        if [[ $line =~ $path ]]; then
+          pathname="${BASH_REMATCH[1]}"
+          pathname=`echo "$pathname" | tr '"' '""'`
+          parts="(issues)\/([^\/]*)\/([0-9]+)\/([^\/]*)\/([^\/]*)\/(.*)\.(.*)$"
+          if [[ $pathname =~ $parts ]]; then
+            publication="${BASH_REMATCH[2]}"
+            year="${BASH_REMATCH[3]}"
+            volis="${BASH_REMATCH[4]}"
+            part="${BASH_REMATCH[5]}"
+            title="${BASH_REMATCH[6]}"
+            extension="${BASH_REMATCH[7]}"
+            url="https://s3.amazonaws.com/seapapers.library.cornell.edu/$pathname"
+            echo "$publication,$year,$volis,$part,$extension,\"$title\",\"$url\""
+          fi
         fi
       fi
-
+    else
+      >&2 echo "REJECTED: $line"
     fi
   done
