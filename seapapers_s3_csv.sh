5546e93eeed753294b387c2ff820b26e3861aa77
James Reidy <jreidy@cornell.edu>
2017-09-01 14:07:50 -0400

    switch to using s3cmd

--- seapapers_s3_csv.sh
+++ seapapers_s3_csv.sh
@@ -2,20 +2,33 @@
 # seapapers_s3_csv.sh - list contents of seapapers.library.cornell.edu/issues as a csv file
 #
 
+# set to s3cmd or aws
+access="s3cmd"
+
 echo "issuekey,partkey,publication,year,volis,part,extension,title,url"
 
-aws s3 ls seapapers.library.cornell.edu/issues/ --recursive | while read line;
+if [ "$access" == "aws" ]; then
+  formcheck="([0-9\-]+) ([0-9\:]+)[ ]+([0-9]+) issues\/([a-z\-]+)\/([0-9\:]+)\/([^\/]+)\/([^\/]+)\/(.+)\.([a-zA-Z]+)$"
+  sizecheck="[0-9]+[0-9] issues\/"
+  path="[0-9]+ (issues\/.*)$"
+  parts="(issues)\/([^\/]*)\/([0-9]+)\/([^\/]*)\/([^\/]*)\/(.*)\.(.*)$"
+else
+  # format returned by s3cmd
+  formcheck="([0-9\-]+)[ ]+([0-9\:]+)[ ]+([0-9]+)[ ]+s3\:\/\/seapapers.library.cornell.edu\/issues\/([a-z\-]+)\/([0-9\:]+)\/([^\/]+)\/([^\/]+)\/(.+)\.([a-zA-Z]+)$"
+  sizecheck="[0-9]+[0-9][ ]+s3\:\/\/seapapers.library.cornell.edu\/issues\/"
+  path="[0-9]+[ ]+s3\:\/\/seapapers.library.cornell.edu\/(issues\/.*)$"
+  parts="(issues)\/([^\/]*)\/([0-9]+)\/([^\/]*)\/([^\/]*)\/(.*)\.(.*)$"
+fi
+
+#aws s3 ls seapapers.library.cornell.edu/issues/ --recursive | while read line;
+s3cmd ls -r s3://seapapers.library.cornell.edu/issues/ | while read line;
   do
-    formcheck="([0-9\-]+) ([0-9\:]+)[ ]+([0-9]+) issues\/([a-z\-]+)\/([0-9\:]+)\/([^\/]+)\/([^\/]+)\/(.+)\.([a-zA-Z]+)$"
     if [[ $line =~ $formcheck ]]; then
-      sizecheck="[0-9]+[0-9] issues\/"
       if [[ $line =~ $sizecheck ]]; then
         # eliminate directories and empty files - size 0
-        path="[0-9]+ (issues\/.*)$"
         if [[ $line =~ $path ]]; then
           pathname="${BASH_REMATCH[1]}"
           pathname=`echo "$pathname" | tr '"' '""'`
-          parts="(issues)\/([^\/]*)\/([0-9]+)\/([^\/]*)\/([^\/]*)\/(.*)\.(.*)$"
           if [[ $pathname =~ $parts ]]; then
             publication="${BASH_REMATCH[2]}"
             year="${BASH_REMATCH[3]}"
@@ -32,7 +45,7 @@ aws s3 ls seapapers.library.cornell.edu/issues/ --recursive | while read line;
           fi
         fi
       fi
-    else
-      >&2 echo "REJECTED: $line"
+    # else
+    #   >&2 echo "REJECTED: $line"
     fi
   done
