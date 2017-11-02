fdb63cf7b6441e7e625a71637ae88e9bd96f2002
James Reidy <jreidy@cornell.edu>
2017-11-02 10:12:29 -0400

    Finished mtimur

--- backup-intip-to-s3.sh
+++ backup-intip-to-s3.sh
@@ -20,7 +20,7 @@ papers=(
 #lpost
 #lrl
 #lrt
-mtimur
+#mtimur
 nrr
 palopo
 pare
@@ -58,9 +58,13 @@ tapanuli
 waspada
 )
 
+# without cd / output looks like this:
+# upload: ../../../../Volumes/Elements/intip/nrr/01_2007/odw/2007-01-11/thumbnails/2007-01-11-0011_tb.jpg to s3://seapapers.library.cornell.edu/archive/intip/nrr/01_2007/odw/2007-01-11/thumbnails/2007-01-11-0011_tb.jpg
 cd /
+
 for PAPER in "${papers[@]}"
 do
+  echo "Use <ctrl> z to stop this script..."
   echo "Paper: $PAPER"
   aws s3 sync "/Volumes/Elements/intip/$PAPER/" "s3://seapapers.library.cornell.edu/archive/intip/$PAPER/"
   echo "Paper Done: $PAPER"
